fd948868b4acd844529213de24c3a209ef8ada27
Surendra234 <surendramahawar873@gmail.com>
2022-06-25 18:48:23 +0530

    Stack peak and pop opreation

--- src/com/bridgelabz/MyLinkedList.java
+++ src/com/bridgelabz/MyLinkedList.java
@@ -6,20 +6,48 @@ public class MyLinkedList {
 
     // Push Method
     public void push(int data) {
+
+
         Node node = new Node(data);
         node.next = head;
         head = node;
     }
 
+    // Pop Method
+    public int pop() {
+
+        if(head == null) {
+            System.out.println("Stack Is Empty !");
+            return 0;
+        }
+        Node temp = head;
+        head = temp.next;
+        return temp.key;
+    }
+
+    public void peak() {
+
+        if(head == null) {
+            System.out.println("Stack Is Empty !");
+            return;
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            System.out.println("\nPeak Element : " +temp.key);
+        }
+    }
+
     // Print Method
     public void print() {
+
         Node temp = head;
-        System.out.println("\nStack : ");
+        System.out.print("\nStack : ");
 
         while (temp != null) {
             System.out.print(temp.key +"->");
             temp = temp.next;
         }
-        System.out.println();
     }
 }
--- src/com/bridgelabz/Stack.java
+++ src/com/bridgelabz/Stack.java
@@ -4,12 +4,17 @@ public class Stack{
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome To Stack Program");
+        System.out.println("\nWelcome To Stack Program");
         MyLinkedList stack = new MyLinkedList();
 
         stack.push(70);
         stack.push(30);
         stack.push(56);
         stack.print();
+        stack.peak();
+        stack.pop();
+        stack.pop();
+        stack.pop();
+        stack.peak();
     }
 }
